d1200db0c3376dc4cec5cd3445f06b6049769a6c
EHFrance <73843012+EHFrance@users.noreply.github.com>
2020-11-07 14:14:42 -0500

    Video Class - Extends Media Class
    Inherited by DVD class.

---
+++ Video.java
@@ -0,0 +1,87 @@
+/* Video.java
+ * Application Project 1 - Video Component
+ * Author: Edward France
+ * Date: 11/4/2020
+ * Inherits from Media.
+ */
+
+public class Video extends Media
+{
+  private Artist[] supportingArtists;
+  private int numActors;
+  private Artist director;
+  private String rating;
+  
+  //Constructors
+  
+  public Video (String title, Artist majorArtist, int playTime, int numPlays, Artist[] sa, int na, Artist dir, String rate)
+  {
+    super (title, majorArtist, playTime, numPlays);
+    supportingArtists = sa;
+    numActors = na;
+    director = dir;
+    rating = rate;
+  }
+  
+  public Video()
+    //Null Constructor
+  {
+    super();
+    supportingArtists = new Artist[];
+    numActors = 0;
+    director = new Artist();
+    rating = " ";
+  }
+  
+  //Accessors
+  public Artist[] getSupportingArtists()
+  {
+    return supportingArtists;
+  }
+  
+  public int getNumActors()
+  {
+    return numActors;
+  }
+  
+  public Artist getDirector()
+  {
+    return director
+  }
+  
+  public String getRating()
+  {
+    return rating;
+  }
+  
+  //ToString
+  public String toString()
+  {
+    return super.toString() + "\nSupporting Artists: " + supportingArtists.toString() + 
+      "\nNumber of Actors: " + numActors + 
+      "\nDirector: " + director.toString() + 
+      "\nRating: " + rating ;
+  }
+  
+  //Print Methods
+  
+  public void printSupportingArtists
+  {
+    System.out.print(supportingArtists.toString());
+  }
+  
+  public void printNumActors
+  {
+    System.out.print(numActors);
+  }
+  
+  public void printDirector
+  {
+    System.out.print(director.toString());
+  }
+  
+  public void printRating
+  {
+    System.out.print(rating);
+  }
+}
