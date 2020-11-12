b2282ca21cd2fa57f482c15806c09b96a0c82e4c
SolomonT <63600658+salem-12@users.noreply.github.com>
2020-11-12 14:40:26 -0500

    Add files via upload

--- Media.java
+++ Media.java
@@ -1,39 +1,41 @@
-// Media.java
+/* Media.java
+ * @author Solomon Tsehay
+ * @since 11/1/20
+ */
 import java.util.Scanner;
 import java.util.Collection;
 public class Media
 {
- private String artist;
- private int numOfPlays;
- private String title;
- private int playTime;
-  private int numItems;
+  //Changed type from String to Artist
+ public String majorArtist;
+ public int numOfPlays;
+ public String title;
+ public int playTime;
+ public int numItems;
  
 Scanner sc = new Scanner(System.in);
  public Media()
  {
-  artist = "";
-  numOfPlays = 0;
-  title = "";
-  playTime =0;
+  //artist = "";
+  //numOfPlays = 0;
+  //title = "";
+  //playTime =0;
  }
 
- public Media(String aArtist, int aNumOfPlays, String aTitle, int aPlayTime)
+ public Media(String aMajorArtist, int aNumOfPlays, String aTitle, int aPlayTime)
  {
-  artist = aArtist;
+  majorArtist = aMajorArtist;
   numOfPlays = aNumOfPlays;
   title = aTitle;
   playTime = aPlayTime;
  }
 
-
- public String getArtist()
- {
-   return artist;
+ public String getMajorArtist() 
+   return majorArtist;
  }
- public void setArtist(String aArtist)
+ public void setMajorArtist(String aMajorArtist) 
  {
-  artist = aArtist;
+  majorArtist = aMajorArtist;
  }
 
  public int getNumOfPlays()
@@ -62,10 +64,10 @@ public void setNumOfPlays(int aNumOfPlays)
 
 public void playMedia()
   { 
-      String title, artist, mediaType; 
+    String title, majorArtist, mediaType; 
       
     System.out.print("Enter artist: ");
-    artist = sc.nextLine();
+    majorArtist = sc.nextLine();
     System.out.println ("Enter the title: ");
     title = sc.nextLine();
     System.out.println("Enter the media type: ");
@@ -76,9 +78,24 @@ public void playMedia()
 public String toString() {
 String result =
  "Title: " + title + 
- "\nArtist: " + artist +
+ "\nArtist: " + majorArtist +
  "\nTime: " + playTime + 
  "\nNumber of plays: " + numOfPlays;
  return result;
+}
+
+// equals
+public boolean equals(Object o)
+    {   boolean eq;
+      if (o instanceof Media)
+      {
+        Media other = (Media) o;
+        eq = super.equals(other) &&
+          other.numOfPlays == numOfPlays && other.playTime == playTime && other.numItems == numItems
+          && other.majorArtist.equals(majorArtist)
+          && other.title.equals(title);
+ }
+        else eq = false;
+ return eq;
 }
 }
